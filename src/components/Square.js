ec4aef75bb7256858d59a9eb4f037fdf0f3305cb
tori-calkins <tori.calkins2705@gmail.com>
2023-09-22 19:04:33 -0700

    bare minimum

--- src/components/Square.js
+++ src/components/Square.js
@@ -2,7 +2,9 @@ import React from 'react'
 
 const Square = (props) => {
   return (
+    <div className= 'grid'>
     <div className="square"></div>
-  )
+    )
+    </div>
 }
 export default Square
